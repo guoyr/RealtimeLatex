607721d927dfd054b2a8b213d5e3d02e194a3bdd
Daniel Alabi <alabid@carleton.edu>
2012-03-31 14:32:40 -0500

    made changes to server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var app = module.exports = require('railway').createServer();
 
 if (!module.parent) {
-    var port = process.env.PORT || 3000
+    var port = process.env.PORT || 3000;
     app.listen(port);
     console.log("Railway server listening on port %d within %s environment", port, app.settings.env);
 }
