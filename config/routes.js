0a089b99b4d75321b620b33875a83a8ab9aef2ad
Daniel Alabi <alabid@carleton.edu>
2012-04-07 15:46:45 -0500

    second commit for users controller changes

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,7 @@
 exports.routes = function (map) {
 
-		map.resources("users");
-
+    map.resources("users");
+    map.get("/", "users#index");
     // Generic routes. Add all your routes below this line
     // feel free to remove generic routes
     map.all(':controller/:action');
