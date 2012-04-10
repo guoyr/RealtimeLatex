9a259331d2c538b53bb51106fb259bb6e929eda8
Robert Guo <robertguo@me.com>
2012-04-10 02:10:56 -0500

    submit

--- config/routes.js
+++ config/routes.js
@@ -4,7 +4,7 @@ exports.routes = function (map) {
     // Generic routes. Add all your routes below this line
 
     map.resources("users");
-    map.get("/", "users#index");
+    map.get("/", "users#submit");
     // feel free to remove generic routes
     map.all(':controller/:action');
     map.all(':controller/:action/:id');
