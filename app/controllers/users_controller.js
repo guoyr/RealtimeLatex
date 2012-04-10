809f15ff31f87b8a308ff948ab4b4b6b11c9e120
Robert Guo <robertguo@me.com>
2012-04-09 22:56:18 -0500

    added submit

--- app/controllers/users_controller.js
+++ app/controllers/users_controller.js
@@ -3,7 +3,26 @@ load("application");
  * 
  * variables that we somehome need to get
  */
- var currUser = "Daniel"; 
+var currUser = "Daniel"; 
+
+var mongoose = requrie('mongoose');
+
+mongoose.connect('mongodb://heroku:heroku@staff.mongohq.com:10090/app3589375');
+
+Schema = mongoose.Schema;
+
+var Document = new Schema({
+	content: String
+});
+
+var User = new Schema({
+	username: String,
+	email: String,
+	password: String,
+	docID: [ObjectId]
+});
+
+var userModel = mongoose.model('User', User);
 
  action(function index() {
  	render({
@@ -24,4 +43,5 @@ load("application");
  action('submit', function() {
  	console.log("submit");
  	render(index.jade);
+ 	u1 = new UserModel();
  });
