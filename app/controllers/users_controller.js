bfaa0220ccfc35acb3de6bdbe9c73de1dfbc75e9
Robert Guo <robertguo@me.com>
2012-04-10 23:04:38 -0500

    s

--- app/controllers/users_controller.js
+++ app/controllers/users_controller.js
@@ -4,25 +4,6 @@ load("application");
  * variables that we somehome need to get
  */
 var currUser = "Daniel"; 
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://heroku:heroku@staff.mongohq.com:10090/app3589375');
-
-Schema = mongoose.Schema;
-
-var Document = new Schema({
-	content: String
-});
-
-var User = new Schema({
-	username: String,
-	email: String,
-	password: String,
-	docID: [Object]
-});
-
-var userModel = mongoose.model('User', User);
-var documentModel = mongoose.model('Document', Document);
 
  action(function index() {
  	console.log("at index");
@@ -31,23 +12,4 @@ var documentModel = mongoose.model('Document', Document);
  		current_user: currUser,
  		in_session: true
  	});
- });
-
-
- action(function submit() {
-/*
- 	console.log("at submit");
- 	
- 	userModel.findOne({'username':req.body.username}, function(err,user){
- 		
- 		if (user == null) {
- 			u1 = new userModel();
- 			u1.username = req.body.username;
- 			u1.passowrd = req.body.password;
- 			u1.save();
- 			console.log("new user saved");
- 		};
- 	});
- */	
- 	render();
  });
